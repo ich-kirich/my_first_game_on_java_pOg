64993062b14229bc1c29211ca0e2313d4a5d4cfd
ich_kirich <tankobaldoytpa@gmail.com>
2022-02-21 22:18:17 +0300

    Add tap-music

--- Flappy Bird/core/src/com/mygdx/game/Bird.java
+++ Flappy Bird/core/src/com/mygdx/game/Bird.java
@@ -2,6 +2,7 @@ package com.mygdx.game;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
+import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
@@ -11,12 +12,14 @@ public class Bird {
 	Vector2 position;
 	float vy;
 	float gravity;
+	Music fly_stepan;
 	
 	public Bird() {
 		img = new Texture("bird2.png");
 		position = new Vector2(100, 380);
 		vy = 0;
 		gravity = -0.7f; // гравитация
+		fly_stepan = Gdx.audio.newMusic(Gdx.files.internal("fly_stepan1.mp3")); // при полете степана
 	}
 	
 	public void render(SpriteBatch batch) {
@@ -26,7 +29,12 @@ public class Bird {
 	public void update() {
 		if(Gdx.input.isKeyPressed(Input.Keys.SPACE)) {
 			vy = 5; // при нажати на пробел птичка прыгает выше
+			fly_stepan.play();
 		}
+		else {
+			fly_stepan.stop();
+		}
+		
 		vy += gravity;
 		position.y += vy;
 	}
